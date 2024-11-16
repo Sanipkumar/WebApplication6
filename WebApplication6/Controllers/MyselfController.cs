7a2b5680e3f7d8080660ac2aaa09c82f9750b771
meheady <hmmehedi55@gmail.com>
2024-11-17 02:49:54 +0600

    Add project files.

---
+++ WebApplication6/Controllers/MyselfController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication6.Controllers
+{
+    public class MyselfController : Controller
+    {
+        // GET: Myself
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
